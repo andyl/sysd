fcefb3236c5f1429dffa7eae9acbc3a75bfe0f46
andyl <andy@r210.com>
2026-03-27 06:54:34 -0700

    chore: generate project

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :git_ops,
   mix_project: Mix.Project.get!(),
   types: [tidbit: [hidden?: true], important: [header: "Important Changes"]],
-  github_handle_lookup?: true,
+  github_handle_lookup?: false,
   version_tag_prefix: "v",
   manage_mix_version?: true,
   manage_readme_version: true
